2e26c06bf98f78186560ab29cc04161f7e519058
Victor Jimenez <vjimenez@wigilabs.com>
2023-04-27 00:35:53 -0500

    dev

---
+++ src/app/dialog/add-cliente/add-cliente.component.ts
@@ -0,0 +1,52 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApiService } from 'app/api.service';
+import { Proveedor } from 'app/modelos/proveedor';
+
+@Component({
+  selector: 'app-add-cliente',
+  templateUrl: './add-cliente.component.html',
+  styleUrls: ['./add-cliente.component.css']
+})
+
+
+
+export class AddClienteComponent implements OnInit {
+
+  constructor(
+    private toastr: MatSnackBar,
+    public dialogRef: MatDialogRef<AddClienteComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Proveedor,
+    private api:ApiService
+
+  ) { }
+
+    /*onLoadDatos(event:any){
+    if(event.target.value!=""){
+    this.api.getProveedor(event.target.value).subscribe(data => {
+      if(data) {
+        console.log(data);
+        this.data.razon_social=data['razonSocial']
+        this.data.direccion=data['direccion']
+        this.data.departamento=data['departamento']
+        this.data.provincia=data['provincia']
+        this.data.distrito=data['distrito']
+      }
+    },
+    error=>{
+      console.log(error)
+      this.toastr.open("Numero de RUC incorrecto","OK");
+    } );
+  }else{
+    this.toastr.open("Debe indicar el Numero de RUC","OK");
+  }
+ }*/
+
+  ngOnInit() {
+  }
+  cancelar() {
+    this.dialogRef.close();
+  }
+
+}
