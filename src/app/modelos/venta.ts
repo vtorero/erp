2d1fc03f41581e77acbfff481aa1a44f30da2416
Victor Jimenez <vjimenez@wigilabs.com>
2023-09-04 21:21:33 -0500

    add

---
+++ src/app/modelos/venta.ts
@@ -0,0 +1,22 @@
+import { Details } from './details';
+export class Venta {
+    constructor(
+        public id:number,
+        public id_usuario:string,
+        public id_vendedor:any,
+        public cliente:any,
+        public	estado:number,
+        public nro_comprobante:string,
+        public fecha:any,
+        public fechaPago:any,
+        public igv:number,
+        public monto_igv:number,
+        public valor_total:number,
+        public detalleVenta:Array<Details>,
+        public imprimir:boolean,
+        public tipoDoc:string,
+        public valor_neto:number,
+        public observacion:string,
+        public formaPago:string
+        ){}
+}
