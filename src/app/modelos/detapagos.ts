0c498699264935fd38a213ace88637ef08a3229a
Victor Jimenez <vjimenez@wigilabs.com>
2023-10-10 00:34:12 -0500

    avance

---
+++ src/app/modelos/detapagos.ts
@@ -0,0 +1,9 @@
+export class DetaPagos {
+    constructor(
+        public id: number,
+        public tipoPago: string,
+        public monto:number,
+        public fecha_registro:string,
+
+){}
+}
