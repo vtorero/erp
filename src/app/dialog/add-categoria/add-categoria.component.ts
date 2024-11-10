463681f33fc775892c11e6fcda0c4e78871184fe
Victor Jimenez <vjimenez@wigilabs.com>
2024-11-10 01:19:25 -0500

    avance

--- src/app/dialog/add-categoria/add-categoria.component.ts
+++ src/app/dialog/add-categoria/add-categoria.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ApiService } from 'app/api.service';
 import { Categoria } from 'app/modelos/categoria';
 import { DetaPagos } from 'app/modelos/detapagos';
 
@@ -9,13 +10,23 @@ import { DetaPagos } from 'app/modelos/detapagos';
   styleUrls: ['./add-categoria.component.css']
 })
 export class AddCategoriaComponent implements OnInit {
-
+dataCategoria:any;
   constructor(
+    private api:ApiService,
     @Inject(MAT_DIALOG_DATA) public data: Categoria
 
   ) { }
 
   ngOnInit(): void {
-  }
+    this.getCate();
 
+  }
+  getCate(): void {
+    this.api.getApi('categorias').subscribe(data => {
+      if(data) {
+        console.log("cate",data)
+        this.dataCategoria = data;
+      }
+    } );
+  }
 }
--- src/app/dialog/add-sub-categoria/add-sub-categoria.component.ts
+++ src/app/dialog/add-sub-categoria/add-sub-categoria.component.ts
@@ -1,6 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ApiService } from 'app/api.service';
 import { Categoria } from 'app/modelos/categoria';
+import { Subcategoria } from 'app/modelos/subcategoria';
 
 @Component({
   selector: 'app-add-sub-categoria',
@@ -8,14 +11,23 @@ import { Categoria } from 'app/modelos/categoria';
   styleUrls: ['./add-sub-categoria.component.css']
 })
 export class AddSubCategoriaComponent implements OnInit {
-
+  dataCategoria:any;
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: Categoria
+    private _snackBar: MatSnackBar,
+    private api:ApiService,
+    @Inject(MAT_DIALOG_DATA) public data: Subcategoria
 
   ) { }
 
   ngOnInit(): void {
-
+this.getCate();
   }
-
+  getCate(): void {
+    this.api.getApi('categorias').subscribe(data => {
+      if(data) {
+        console.log("cate",data)
+        this.dataCategoria = data;
+      }
+    } );
+  }
 }
